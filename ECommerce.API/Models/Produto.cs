11657e5307f8359d0a67ea6e25f31719aac525e5
Giovanna Gamba <giovannagamba9@gmail.com>
2024-04-15 11:08:27 -0300

    Redefine regra de Produto

--- ECommerce.API/Models/Produto.cs
+++ ECommerce.API/Models/Produto.cs
@@ -17,4 +17,28 @@ public class Produto
     [Column(TypeName = "decimal(10,2)")]
     public decimal Preco { get; set; }
     public float Estoque { get; set; }
+
+    private Produto()
+    {
+
+    }
+
+    public Produto(int produtoId, string? nome, string? descricao, decimal preco, float estoque)
+    {
+        ProdutoId=produtoId;
+        Nome=nome;
+        Descricao=descricao;
+        Preco=preco;
+        Estoque=estoque;
+
+        ValidarPreco();
+    }
+
+    public void ValidarPreco()
+    {
+        if (Preco < 0)
+        {
+            throw new Exception("Preço não pode ser menor que 0,00.");
+        }
+    }
 }
